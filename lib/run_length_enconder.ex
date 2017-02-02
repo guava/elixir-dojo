f87870081d32604f6a2d6866c547a6c483ee3ae5
Daniel Zanzini <daniel@guava.com.br>
2017-02-02 19:00:37 -0300

    Finishes Run Lenght Encoder problem :godmode:

--- lib/run_length_enconder.ex
+++ lib/run_length_enconder.ex
@@ -29,38 +29,56 @@ defmodule RunLengthEncoder do
     "#{encoded_str}#{count}#{<< char >>}"
   end
 
-  def decode(encoded_str) when is_binary(encoded_str) do
+  def decode(encoded_str) do
+    do_decode(encoded_str, "")
+  end
+
+  defp do_decode(encoded_str, decoded_str) when is_binary(encoded_str) do
     encoded_str
-    |> to_char_list
-    |> decode
+    |> Integer.parse
+    |> do_decode(decoded_str)
   end
 
-  def decode([head | tail]) do
-    do_decode(head, tail, '', "")
+  defp do_decode(:error, decoded_str) do
+    decoded_str
   end
 
-  ####                                             ####
-  ## TEM MELHORIA                                    ##
-  ## Usar Integer.parse pra fazer o decode           ##
-  ## Vai evitar verificar se o próximo char é número ##
-  ## GOSTAMOS!                                       ##
-  ####                                             ####
-
-  defp do_decode(char, [head | tail], seen, decoded_str) do
-    if char >= ?0 && char <= ?9 do
-      do_decode(head, tail, "#{seen}#{<< char >>}", decoded_str)
-    else
-      new = List.duplicate([char], String.to_integer(seen))
-      do_decode(head, tail, '', "#{decoded_str}#{new}")
-    end
+  defp do_decode({number, encoded_str}, decoded_str) do
+    encoded_str
+    |> String.split_at(1)
+    |> do_decode(number, decoded_str)
   end
 
-  defp do_decode(char, [], seen, decoded_str) do
-    if Integer.parse(seen) >= 0 do
-      new = List.duplicate([char], String.to_integer(seen))
-      "#{decoded_str}#{new}"
-    else
-      decoded_str
-    end
+  defp do_decode({letter, encoded_str}, number, decoded_str) do
+    do_decode(encoded_str, "#{decoded_str}#{List.duplicate([letter], number)}")
   end
+
+  # ALTERNATIVE VERSION:
+  # def decode(encoded_str) when is_binary(encoded_str) do
+  #   encoded_str
+  #   |> to_char_list
+  #   |> decode
+  # end
+
+  # def decode([head | tail]) do
+  #   do_decode(head, tail, '', "")
+  # end
+
+  # defp do_decode(char, [head | tail], seen, decoded_str) do
+  #   if char >= ?0 && char <= ?9 do
+  #     do_decode(head, tail, "#{seen}#{<< char >>}", decoded_str)
+  #   else
+  #     new = List.duplicate([char], String.to_integer(seen))
+  #     do_decode(head, tail, '', "#{decoded_str}#{new}")
+  #   end
+  # end
+
+  # defp do_decode(char, [], seen, decoded_str) do
+  #   if Integer.parse(seen) >= 0 do
+  #     new = List.duplicate([char], String.to_integer(seen))
+  #     "#{decoded_str}#{new}"
+  #   else
+  #     decoded_str
+  #   end
+  # end
 end
