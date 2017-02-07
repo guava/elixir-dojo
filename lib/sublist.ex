beac7fb41741039dbcada0c708b9be5f29f545ed
Daniel Zanzini <daniel@guava.com.br>
2017-02-07 18:48:23 -0300

    Solves Sublist problem.

--- lib/sublist.ex
+++ lib/sublist.ex
@@ -1,17 +1,41 @@
 defmodule Sublist do
-  def compare([], []), do: :equal
-  def compare([], list_b), do: :sublist
-  def compare(list_a, []), do: :superlist
 
-  def compare([head_a | []], [head_b | []]) when head_a != head_b do
+  def compare(list_a, list_b) when length(list_a) > length(list_b) do
+    result = compare_sublist(list_b, list_a)
+
+    if result == :sublist do
+      :superlist
+    else
+      result
+    end
+  end
+
+  def compare(list_a, list_b) when length(list_a) < length(list_b) do
+    compare_sublist(list_a, list_b)
+  end
+
+  def compare(list_a, list_b) do
+    check_equality(list_a, list_b)
+  end
+
+  def check_equality([], []), do: :equal
+
+  def check_equality([head_a | []], [head_b | []]) when head_a !== head_b do
     :unequal
   end
 
-  def compare([head_a | tail_a], [head_b | tail_b]) when head_a == head_b do
-    compare(tail_a, tail_b)
+  def check_equality([head_a | tail_a], [head_b | tail_b]) when head_a === head_b do
+    check_equality(tail_a, tail_b)
   end
 
-  def compare(list_a, [head_b | tail_b]) do
-    compare(list_a, tail_b)
+  defp compare_sublist([], _list_b), do: :sublist
+  defp compare_sublist(_list_a, []), do: :unequal
+
+  defp compare_sublist([head_a | tail_a], [head_b | tail_b]) when head_a === head_b do
+    compare_sublist(tail_a, tail_b)
+  end
+
+  defp compare_sublist(list_a, [head_b | tail_b]) do
+    compare_sublist(list_a, tail_b)
   end
 end
